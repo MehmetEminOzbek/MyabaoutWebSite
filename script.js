318e3e55d4f080b0c57c07105bb851b793defffd
MehmetEminOzbek <124729763+MehmetEminOzbek@users.noreply.github.com>
2023-03-08 13:46:25 +0300

    MyAbout

---
+++ script.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".multiple-text",{
+    strings:["Frontend Developer","BackEnd Developer","Student","Full Stack Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
